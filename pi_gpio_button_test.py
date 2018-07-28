6341afce3110149a6dedc40888e685f2c4519107
blingbang <mattkrll@gmail.com>
2018-07-28 21:15:51 +0200

    fixed pi_gpio_button_test.py

--- pi_gpio_button_test.py
+++ pi_gpio_button_test.py
@@ -2,14 +2,14 @@ import RPi.GPIO as gpio
 import time
 
 
-def gpio_callback():
+def gpio_callback(channel):
     print("Button Pushed")
 
 
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
 gpio.setup(11, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.add_event_detect(11, gpio.RISING, callback=gpio_callback())
+gpio.add_event_detect(11, gpio.RISING, callback=gpio_callback)
 
 while True:
     time.sleep(4)
